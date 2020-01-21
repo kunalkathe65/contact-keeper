5a6b9ed54d8d46e303e64325340191f6324eb1e7
Kunal Kathe <kunal.kathe@volansys.com>
2020-01-21 15:13:03 +0530

    added JWT for sending a token when  user register

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect Database
+//Connect Database
 connectDB();
 
-const PORT= process.env.PORT | 5000;
+//first it will look for a environment variable "PORT" (in production env) or else will start a server on 5000
+const PORT= process.env.PORT | 5000;   
 
 //Init Middleware
 app.use(express.json({ extended:false})); 
