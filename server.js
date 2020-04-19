72cd183737a8863d2e8c744b65d14dfefec3501e
kunalkathe65 <kunalkathe65@gmail.com>
2020-04-19 21:30:19 +0530

    Prepare to deploy...

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -7,17 +8,26 @@ const app = express();
 connectDB();
 
 //first it will look for a environment variable "PORT" (in production env) or else will start a server on 5000
-const PORT= process.env.PORT | 5000;   
+const PORT = process.env.PORT | 5000;
 
 //Init Middleware
-app.use(express.json({ extended:false})); 
+app.use(express.json({ extended: false }));
 
 //Defining routes
-app.use('/api/user',require('./routes/user'));
-app.use('/api/contact',require('./routes/contact'));
-app.use('/api/auth',require('./routes/auth'));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/contact", require("./routes/contact"));
+app.use("/api/auth", require("./routes/auth"));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
 
-app.listen(PORT, () =>{
-    console.log(`Server started on port ${PORT}`);
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
